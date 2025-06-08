b0657307b46f8778972214476fd9c7b4e80a514f
Hussain Aminu <hussain_aminu@yahoo.com>
2025-06-08 18:22:46 +0300

    Remove unused imports in App component and add Apollo Client setup in main entry point

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
