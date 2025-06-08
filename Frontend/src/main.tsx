8d47284e063c7d21195e9d3a16390a37f7b12ea4
Hussain Aminu <hussain_aminu@yahoo.com>
2025-06-08 19:28:20 +0300

    Refactor App component: remove unused state and enhance main entry point with Apollo Client setup and initial GraphQL query

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,8 +4,28 @@ import './index.css'
 import App from './App.tsx'
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  // uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query games {
+        games {
+          id
+          title
+          platform
+        }
+      }
+      `,
+  })
+  .then((result) => console.log(result));
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
+  </ApolloProvider>,
 )
